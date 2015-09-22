9ed5f5f3ffeb5dec2cad2a6aa066c4fa8b6dbaae
MB Burch <marybethburch@gmail.com>
2015-09-22 17:12:43 -0600

    fixed hound issues with hash indentations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 loan1 = Loan.create(title: "Urban Bees",
-            description: "Buy local Denver honey.",
-            price: 50)
+                    description: "Buy local Denver honey.",
+                    price: 50)
 
 loan2 = Loan.create(title: "Kitten Mittens",
-            description: "Keep your kittens cozy.",
-            price: 25)
+                    description: "Keep your kittens cozy.",
+                    price: 25)
 
 loan3 = Loan.create(title: "Bike Bus",
-            description: "We'll bring our shop to you.",
-            price: 100)
+                    description: "We'll bring our shop to you.",
+                    price: 100)
 
 category1 = Category.create(name: "Crafts")
 
--- spec/models/loan_spec.rb
+++ spec/models/loan_spec.rb
@@ -2,26 +2,25 @@ require "rails_helper"
 
 RSpec.describe Loan, type: :model do
   loan = Loan.new(title: "Urban Bees",
-             description: "Buy local Denver honey.",
-             price: 50)
+                  description: "Buy local Denver honey.",
+                  price: 50)
 
-    it "is valid" do
-      expect(loan).to be_valid
-    end
+  it "is valid" do
+    expect(loan).to be_valid
+  end
 
-    it "is invalid without a title" do
-      loan.title = nil
-      expect(loan).to_not be_valid
-    end
+  it "is invalid without a title" do
+    loan.title = nil
+    expect(loan).to_not be_valid
+  end
 
-    it "is invalid without a description" do
-      loan.description = nil
-      expect(loan).to_not be_valid
-    end
+  it "is invalid without a description" do
+    loan.description = nil
+    expect(loan).to_not be_valid
+  end
 
-    it "is invalid without a price" do
-      loan.price = nil
-      expect(loan).to_not be_valid
-    end
+  it "is invalid without a price" do
+    loan.price = nil
+    expect(loan).to_not be_valid
+  end
 end
-
