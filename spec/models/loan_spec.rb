35200dc7196f5cb14d9d988467acc332d6978151
MB Burch <marybethburch@gmail.com>
2015-09-22 17:04:56 -0600

    added model tests and seed data

--- spec/models/loan_spec.rb
+++ spec/models/loan_spec.rb
@@ -1,5 +1,27 @@
 require "rails_helper"
 
 RSpec.describe Loan, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  loan = Loan.new(title: "Urban Bees",
+             description: "Buy local Denver honey.",
+             price: 50)
+
+    it "is valid" do
+      expect(loan).to be_valid
+    end
+
+    it "is invalid without a title" do
+      loan.title = nil
+      expect(loan).to_not be_valid
+    end
+
+    it "is invalid without a description" do
+      loan.description = nil
+      expect(loan).to_not be_valid
+    end
+
+    it "is invalid without a price" do
+      loan.price = nil
+      expect(loan).to_not be_valid
+    end
 end
+
