d901ea7bd2ba14787ac1c92c1c9bdf4602f33f18
Chris Cenatiempo <etothe2pieye@gmail.com>
2015-09-24 17:56:02 -0600

    Changes made to support paperclip

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class Loan < ActiveRecord::Base
   validates :title, presence: true
   validates :description, presence: true
@@ -8,4 +10,9 @@ class Loan < ActiveRecord::Base
                     styles: { medium: "800x300#", thumb: "320x150#" },
                     default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
+  def avatar_remote_url(url)
+    self.avatar = URI.parse(url)
+    @avatar_remote_url
+  end
 end
