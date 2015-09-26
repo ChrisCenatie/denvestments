4bd387abc2e14212f54bcac38474190f3268c69a
Chris Cenatiempo <etothe2pieye@gmail.com>
2015-09-26 02:31:18 -0600

    Loans can now be added to cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -3,7 +3,11 @@ class CartItemsController < ApplicationController
     loan_cart = cart
     loan_cart.add_item(params)
     session[:cart] = loan_cart.data
-    redirect_to loans_path
+    if params[:path] == "category_show"
+      redirect_to category_path(params[:category_id])
+    else
+      redirect_to loans_path
+    end
   end
 
   def index
