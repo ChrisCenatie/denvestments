be963ca0c6c98c5bbfd9596e066ef58cc6e764cb
Jeffrey Ruane <ruane.jb@gmail.com>
2015-09-26 13:53:44 -0600

    User can't create an account without a unique username

--- spec/features/visitor_creates_account_spec.rb
+++ spec/features/visitor_creates_account_spec.rb
@@ -14,4 +14,16 @@ feature "Visitor creating account" do
     expect(page).not_to have_content("Create Account")
     expect(page).not_to have_content("login")
   end
+
+  scenario "won't work with invalid information" do
+    User.create(username: "alice", password: "password")
+    visit "/"
+    click_link "Create Account"
+    fill_in "user[username]", with: "alice"
+    fill_in "user[password]", with: "password"
+    click_button "Submit"
+
+    expect(current_path).to eq("/register")
+    expect(page).to have_content("Username has already been taken")
+  end
 end
