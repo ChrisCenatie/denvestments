624369dd7ff385bfa427b554e9821a09d3f8493d
russelleh <russell.west.eh@gmail.com>
2015-09-26 14:42:53 -0600

    Delete douplicate spec

--- spec/features/visitor_redirected_from_dashboard.rb
+++
@@ -1,8 +0,0 @@
-require "rails_helper"
-
-feature "Visitor visiting dashboard" do
-  scenario "is redirected when not logged in" do
-    visit "/dashboard"
-    expect(current_path).to eq("/")
-  end
-end
--- spec/features/visitor_redirected_from_dashboard_spec.rb
+++ spec/features/visitor_redirected_from_dashboard_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-feature "visitor visiting dashboard" do
+feature "Visitor visiting dashboard" do
   scenario "is redirected when not logged in" do
     visit "/dashboard"
     expect(current_path).to eq("/")
