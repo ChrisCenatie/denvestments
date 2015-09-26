3d89726e3a778621556fe609f73a318bdee9749a
russelleh <russell.west.eh@gmail.com>
2015-09-26 11:27:51 -0600

    User has a password digest instead of a password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe User, type: :model do
     user = User.create(username: "bob", password: "don't tell nobody")
     expect(User.find_by(username: "bob")).to eq(user)
   end
+
+  it "has a password digest instead of a password" do
+    User.create(username: "alice", password: "english")
+    user = User.find_by(username: "alice")
+    expect(user.password).to eq(nil)
+    expect(user.password_digest).not_to eq("english")
+  end
 end
