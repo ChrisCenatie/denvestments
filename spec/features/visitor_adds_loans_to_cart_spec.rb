e6a1c3e5f4ca60adaa0d0bfd74902944edb54c31
MB Burch <marybethburch@gmail.com>
2015-09-26 20:28:23 -0600

    Added loan show page and tests. Needs CSS

--- spec/features/visitor_adds_loans_to_cart_spec.rb
+++ spec/features/visitor_adds_loans_to_cart_spec.rb
@@ -37,7 +37,27 @@ feature "visitor adds loans to shopping cart" do
       click_button("Add to Cart")
     end
     expect(page).to have_content("Loans in Cart: 1")
-    expect(page.current_path).to eq "/categories/#{category.id}"
+    expect(current_path).to eq "/categories/#{category.id}"
+    page.find("#cart").click
+    within(".items") do
+      expect(page).to have_content("Kitten Mittens")
+      expect(page).to have_content("Support your kittens!")
+      expect(page).to have_content("Total Price: 5")
+      # expect(page).to have_css("img")
+    end
+  end
+
+  scenario "without being logged in from the loan show page" do
+    loan = Loan.create(title: "Kitten Mittens",
+                       description: "Support your kittens!",
+                       price: 5,
+                       avatar: File.open("bird.jpg", "rb"))
+
+    visit "/loans/#{loan.id}"
+    click_button("Add to Cart")
+
+    expect(page).to have_content("Loans in Cart: 1")
+    expect(current_path).to eq "/loans/#{loan.id}"
     page.find("#cart").click
     within(".items") do
       expect(page).to have_content("Kitten Mittens")
