6d42e2389a1752cbbd6cd6b15e5c60f8e13b2cc5
MB Burch <marybethburch@gmail.com>
2015-09-28 16:03:36 -0600

    add full name and address to tests and new user views

--- spec/features/visitor_creates_account_spec.rb
+++ spec/features/visitor_creates_account_spec.rb
@@ -6,6 +6,8 @@ feature "Visitor creating account" do
     click_link "Create Account"
     fill_in "user[username]", with: "grant"
     fill_in "user[password]", with: "password"
+    fill_in "user[full_name]", with: "Grant Jones"
+    fill_in "user[address]", with: "1500 Blake St., Denver, CO 80205"
     click_button "Submit"
 
     expect(current_path).to eq("/dashboard")
@@ -16,11 +18,13 @@ feature "Visitor creating account" do
   end
 
   scenario "won't work with invalid information" do
-    User.create(username: "alice", password: "password")
+    User.create(username: "alice", password: "password", full_name: "Alice Jones", address: "1500 Blake St., Denver, CO 80205")
     visit "/"
     click_link "Create Account"
     fill_in "user[username]", with: "alice"
     fill_in "user[password]", with: "password"
+    fill_in "user[full_name]", with: "Alice Jones"
+    fill_in "user[address]", with: "1500 Blake St., Denver, CO 80205"
     click_button "Submit"
 
     expect(current_path).to eq("/register")
