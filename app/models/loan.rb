4b634ca6176060c8a23d8adffa78ef4915a7baf0
russelleh <russell.west.eh@gmail.com>
2015-09-28 14:34:23 -0600

    Loan must have a price above zero

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -3,7 +3,7 @@ require "open-uri"
 class Loan < ActiveRecord::Base
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
 
   belongs_to :category
   has_attached_file :avatar,
--- spec/models/loan_spec.rb
+++ spec/models/loan_spec.rb
@@ -1,38 +1,43 @@
 require "rails_helper"
 
 RSpec.describe Loan, type: :model do
-  loan = Loan.new(title: "Urban Bees",
-                  description: "Buy local Denver honey.",
-                  price: 50)
+  before(:each) do
+    @loan = Loan.new(title: "Urban Bees",
+                    description: "Buy local Denver honey.",
+                    price: 50)
+  end
 
   it "is valid" do
-    expect(loan).to be_valid
+    expect(@loan).to be_valid
   end
 
   it "is invalid without a title" do
-    loan.title = nil
-    expect(loan).to_not be_valid
+    @loan.title = nil
+    expect(@loan).to_not be_valid
   end
 
   it "is invalid without a description" do
-    loan.description = nil
-    expect(loan).to_not be_valid
+    @loan.description = nil
+    expect(@loan).to_not be_valid
   end
 
   it "is invalid without a price" do
-    loan.price = nil
-    expect(loan).to_not be_valid
+    @loan.price = nil
+    expect(@loan).to_not be_valid
   end
 
   it "must have a unique name" do
-    old_loan = Loan.new(title: "Test",
+    @loan.save
+    new_loan = Loan.new(title: "Urban Bees",
                     description: "Buy local Denver honey.",
                     price: 50)
-    old_loan.save
-    new_loan = Loan.new(title: "Test",
-                    description: "Buy local Denver honey.",
-                    price: 50)
-    expect(old_loan).to be_valid
     expect(new_loan).to_not be_valid
+  end
+
+  it "must have a price above zero" do
+    @loan.price = 0
+    expect(@loan).to_not be_valid
+    @loan.price = 1
+    expect(@loan).to be_valid
   end
 end
