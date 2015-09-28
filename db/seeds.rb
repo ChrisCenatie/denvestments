b89692b911099aa94fd5227d1c682af281118cb3
Jeffrey Ruane <ruane.jb@gmail.com>
2015-09-28 15:20:41 -0600

    Use free images instead of copyrighted images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
 loan1 = Loan.create(title: "Urban Bees",
                     description: "Buy local Denver honey.",
                     price: 50,
-                    avatar_file_name: "http://i.dailymail.co.uk/i/pix/2011/08/01/article-2021108-0D40723300000578-13_634x383.jpg")
+                    avatar_file_name: "https://s3-us-west-2.amazonaws.com/turingdenvestments/assets/Fork-Beekeeping.jpg")
 
 loan2 = Loan.create(title: "Kitten Mittens",
                     description: "Keep your kittens cozy. These mittens are handcrafted by a Capitol Hill artisan who anticipates being able to repay the loan within 6 months of starting her business. She already has a proven customer base.",
                     price: 25,
-                    avatar_file_name: "https://s3-us-west-2.amazonaws.com/turingdenvestments/3_Kittens_Playing_in_the_Garden.jpg")
+                    avatar_file_name: "https://s3-us-west-2.amazonaws.com/turingdenvestments/assets/3_Kittens_Playing_in_the_Garden.jpg")
 
 loan3 = Loan.create(title: "Bike Bus",
                     description: "We'll bring our shop to you.",
                     price: 100,
-                    avatar_file_name: "http://urbansimplicty.files.wordpress.com/2009/06/bikebus.jpg")
+                    avatar_file_name: "https://s3-us-west-2.amazonaws.com/turingdenvestments/assets/Hilltop_Bicycles_Summit_NJ.JPG")
 
 category1 = Category.create(name: "Crafts")
 
