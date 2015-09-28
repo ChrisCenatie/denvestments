004ec998487583c4a155d5266c9a965a4577cd5f
russelleh <russell.west.eh@gmail.com>
2015-09-28 14:48:23 -0600

    User must have a full name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
+  validates :full_name, presence: true
   has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,32 +1,39 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
+  before(:each) do
+    @user = User.new(username: "bob", password: "secret", full_name: "Bob Doe")
+  end
+
   it "can be created with normal attributes" do
-    user = User.create(username: "bob", password: "don't tell nobody")
-    expect(User.find_by(username: "bob")).to eq(user)
+    expect(@user).to be_valid
   end
 
   it "has a password digest instead of a password" do
-    User.create(username: "alice", password: "english")
-    user = User.find_by(username: "alice")
+    @user.save
+    user = User.last
     expect(user.password).to eq(nil)
-    expect(user.password_digest).not_to eq("english")
+    expect(user.password_digest).not_to eq("secret")
   end
 
   it "must have a username" do
-    user = User.new(password: "secret")
-    refute(user.save)
+    @user = User.new(password: "secret", full_name: "Bob Doe")
+    expect(@user).not_to be_valid
   end
 
   it "must have a password" do
-    user = User.new(username: "alice")
-    refute(user.save)
+    @user = User.new(username: "bob", full_name: "Bob Doe")
+    expect(@user).not_to be_valid
   end
 
   it "cannot have the same username as another user" do
-    user_1 = User.new(username: "alice", password: "secret_1")
-    user_2 = User.new(username: "alice", password: "secret_2")
-    expect(user_1.save)
-    refute(user_2.save)
+    @user.save
+    new_user = User.new(username: "bob", password: "secret", full_name: "Bob Doe")
+    expect(new_user).not_to be_valid
+  end
+
+  it "must have a full name" do
+    @user = User.new(username: "bob", password: "secret")
+    expect(@user).not_to be_valid
   end
 end
