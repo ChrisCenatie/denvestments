53ede291c5026e905361d0ea37480b652244ec7f
Jeffrey Ruane <ruane.jb@gmail.com>
2015-09-28 17:28:33 -0600

    Fix order and user relationship.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :orders
   validates :username, presence: true, uniqueness: true
   validates :full_name, presence: true
   validates :address, presence: true
