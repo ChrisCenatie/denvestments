8c2b15d783c3b791fdfd29ec3b35c919971d9e41
Jeffrey Ruane <ruane.jb@gmail.com>
2015-09-29 09:50:37 -0600

    Non admin can not view admin dashboard.

--- spec/features/admin_can_see_dashboard_spec.rb
+++ spec/features/admin_can_see_dashboard_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
-feature "An admin logging in" do
-  scenario "can see the admin dashboard" do
+feature "A visitor logging in" do
+  scenario "can see the admin dashboard if admin" do
     User.create(username: "admin",
                 password: "password",
                 full_name: "Admin Adminerstein",
@@ -16,5 +16,22 @@ feature "An admin logging in" do
 
     expect(current_path).to eq("/admin/dashboard")
     expect(page).to have_content("admin")
+  end
+
+  scenario "can not see the admin dashboard if not an admin" do
+    User.create(username: "alice",
+                password: "password",
+                full_name: "Alice Jones",
+                address: "123 Main St.")
+    visit "/"
+    click_link ("Log In")
+    fill_in "user[username]", with: "alice"
+    fill_in "user[password]", with: "password"
+    click_button "Log In"
+
+    expect(current_path).to eq("/dashboard")
+
+    visit "/admin/dashboard"
+    expect(page).to have_content("The page you were looking for doesn't exist")
   end
 end
