5e0322e5f9c8ae7d671f36728e7b7761c60d7d3d
Jeffrey Ruane <ruane.jb@gmail.com>
2015-09-28 18:14:53 -0600

    Add role to user table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150928231525) do
+ActiveRecord::Schema.define(version: 20150929001351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,10 +57,11 @@ ActiveRecord::Schema.define(version: 20150928231525) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.string   "full_name"
     t.string   "address"
+    t.integer  "role",            default: 0
   end
 
   add_foreign_key "loans", "categories"
