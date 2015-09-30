13c179388adf9549e467db8856b7ef4aabf150b4
russelleh <russell.west.eh@gmail.com>
2015-09-30 17:22:27 -0600

    Refactor admin can edit own attributes feature

--- spec/support/features.rb
+++ spec/support/features.rb
@@ -7,6 +7,14 @@ shared_context "features" do
                 role: 1)
   end
 
+  let!(:user) do
+    User.create(username: "alice",
+                password: "password",
+                full_name: "Alice Smith",
+                address: "123 Main St. Anytown, USA",
+                role: 0)
+  end
+
   let!(:loan) do
     category = Category.create(name: "test category")
     loan = Loan.create(title: "test title",
