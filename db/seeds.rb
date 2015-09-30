cca2f8dfedb07d4dbdaf07443f789e2caf937f2f
MB Burch <marybethburch@gmail.com>
2015-09-30 16:32:19 -0600

    change seed data to correct images and categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,14 @@ category3 = Category.create(name: "Transportation")
 Loan.create(title: "Urban Bees",
             description: "Buy local Denver honey.",
             price: 50,
-            avatar: open("https://s3.amazonaws.com/turing-denvestments/assets/3_Kittens_Playing_in_the_Garden.jpg"),
-            category: category1)
+            avatar: open("https://s3.amazonaws.com/turing-denvestments/assets/Fork-Beekeeping.jpg"),
+            category: category2)
 
 Loan.create(title: "Kitten Mittens",
             description: "Keep your kittens cozy. These mittens are handcrafted by a Capitol Hill artisan who anticipates being able to repay the loan within 6 months of starting her business. She already has a proven customer base.",
             price: 25,
             avatar: open("https://s3.amazonaws.com/turing-denvestments/assets/3_Kittens_Playing_in_the_Garden.jpg"),
-            category: category2)
+            category: category1)
 
 Loan.create(title: "Bike Bus",
             description: "We'll bring our shop to you.",
@@ -35,7 +35,7 @@ Order.create(user_id: 1,
              status: "Ordered",
              card_number: "1234123412341234",
              card_expiration: "08/20",
-             total_cost: "50.00")
+             total_cost: 50)
 
 OrderItem.create(loan_id: 1, order_id: 1, quantity: 3)
 OrderItem.create(loan_id: 2, order_id: 1, quantity: 1)
