b54f7578438c80458d9ceb7a7763969f0b3edbf6
Chris Cenatiempo <etothe2pieye@gmail.com>
2015-09-30 14:01:02 -0600

    This completes Admin Orders #77

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,6 +3,10 @@ class Admin::OrdersController < Admin::BaseController
     order = Order.find(params[:id])
     if params[:status] == "cancelled"
       order.update(status: "Cancelled")
+    elsif params[:status] == "paid"
+      order.update(status: "Paid")
+    elsif params[:status] == "completed"
+      order.update(status: "Completed")
     end
     redirect_to admin_dashboard_path
   end
--- spec/features/admin_can_see_dashboard_spec.rb
+++ spec/features/admin_can_see_dashboard_spec.rb
@@ -69,7 +69,7 @@ feature "A visitor logging in" do
     expect(page).to have_content("Ordered - 1")
   end
 
-  scenario "can update order status if admin" do
+  scenario "can update ordered status to cancelled if admin" do
     User.create(username: "admin",
                 password: "password",
                 full_name: "Admin Adminerstein",
@@ -104,6 +104,120 @@ feature "A visitor logging in" do
     within("#Cancelled") do
       expect(page).to have_content(order.id)
       expect(page).to have_content("Cancelled - 1")
+    end
+  end
+
+  scenario "can update ordered status to paid if admin" do
+    User.create(username: "admin",
+                password: "password",
+                full_name: "Admin Adminerstein",
+                address: "123 Admin Blvd, Admintown, USA",
+                role: 1)
+    user = User.create(username: "Chris",
+                       password: "password",
+                       full_name: "Bob C",
+                       address: "9 Sherman",
+                       role: 1)
+    category = Category.create(name: "Cat Stuff")
+    loan = Loan.create(title: "Kitten Mittens",
+                       description: "Everyone needs them!",
+                       price: 100.5,
+                       category_id: category.id)
+    order = Order.create(user_id: user.id,
+                         card_number: "1234",
+                         card_expiration: "09/10",
+                         total_cost: 100.50,
+                         status: "Ordered")
+    OrderItem.create(loan_id: loan.id,
+                     quantity: 1,
+                     order_id: order.id)
+    visit "/"
+    click_link("Log In")
+    fill_in "user[username]", with: "admin"
+    fill_in "user[password]", with: "password"
+    click_button "Log In"
+    click_button("Ordered")
+    click_link("Mark as Paid")
+    click_button("Paid")
+    within("#Paid") do
+      expect(page).to have_content(order.id)
+      expect(page).to have_content("Paid - 1")
+    end
+  end
+
+  scenario "can update paid order status to cancelled if admin" do
+    User.create(username: "admin",
+                password: "password",
+                full_name: "Admin Adminerstein",
+                address: "123 Admin Blvd, Admintown, USA",
+                role: 1)
+    user = User.create(username: "Chris",
+                       password: "password",
+                       full_name: "Bob C",
+                       address: "9 Sherman",
+                       role: 1)
+    category = Category.create(name: "Cat Stuff")
+    loan = Loan.create(title: "Kitten Mittens",
+                       description: "Everyone needs them!",
+                       price: 100.5,
+                       category_id: category.id)
+    order = Order.create(user_id: user.id,
+                         card_number: "1234",
+                         card_expiration: "09/10",
+                         total_cost: 100.50,
+                         status: "Paid")
+    OrderItem.create(loan_id: loan.id,
+                     quantity: 1,
+                     order_id: order.id)
+    visit "/"
+    click_link("Log In")
+    fill_in "user[username]", with: "admin"
+    fill_in "user[password]", with: "password"
+    click_button "Log In"
+    click_button("Paid")
+    click_link("Cancel")
+    click_button("Cancelled")
+    within("#Cancelled") do
+      expect(page).to have_content(order.id)
+      expect(page).to have_content("Cancelled - 1")
+    end
+  end
+
+  scenario "can update paid order status to completed if admin" do
+    User.create(username: "admin",
+                password: "password",
+                full_name: "Admin Adminerstein",
+                address: "123 Admin Blvd, Admintown, USA",
+                role: 1)
+    user = User.create(username: "Chris",
+                       password: "password",
+                       full_name: "Bob C",
+                       address: "9 Sherman",
+                       role: 1)
+    category = Category.create(name: "Cat Stuff")
+    loan = Loan.create(title: "Kitten Mittens",
+                       description: "Everyone needs them!",
+                       price: 100.5,
+                       category_id: category.id)
+    order = Order.create(user_id: user.id,
+                         card_number: "1234",
+                         card_expiration: "09/10",
+                         total_cost: 100.50,
+                         status: "Paid")
+    OrderItem.create(loan_id: loan.id,
+                     quantity: 1,
+                     order_id: order.id)
+    visit "/"
+    click_link("Log In")
+    fill_in "user[username]", with: "admin"
+    fill_in "user[password]", with: "password"
+    click_button "Log In"
+    click_button("Paid")
+    click_link("Mark as Completed")
+    click_button("Completed")
+    within("#Completed") do
+      expect(page).to have_content(order.id)
+      expect(page).to have_content("Completed - 1")
     end
   end
 end
