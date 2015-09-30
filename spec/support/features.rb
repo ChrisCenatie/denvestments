7e15a697bf712ef504643eea611292fb525939b0
russelleh <russell.west.eh@gmail.com>
2015-09-30 17:18:26 -0600

    Break out feature test stuff

---
+++ spec/support/features.rb
@@ -0,0 +1,25 @@
+shared_context "features" do
+  let!(:admin) do
+    User.create(username: "admin",
+                password: "password",
+                full_name: "Admin Adminerstein",
+                address: "123 Admin Blvd, Admintown, USA",
+                role: 1)
+  end
+
+  let!(:loan) do
+    category = Category.create(name: "test category")
+    loan = Loan.create(title: "test title",
+                       description: "test description",
+                       price: 50,
+                       category: category)
+  end
+
+  def log_in_as(username, password)
+    visit "/"
+    click_link("Log In")
+    fill_in "user[username]", with: username
+    fill_in "user[password]", with: password
+    click_button "Log In"
+  end
+end
