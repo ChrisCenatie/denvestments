e1699ab662b1713d17b225fc11dd561f2ac23b94
Jeffrey Ruane <ruane.jb@gmail.com>
2015-09-30 11:40:00 -0600

    Update admin dashboard test to include order index

--- spec/features/admin_can_see_dashboard_spec.rb
+++ spec/features/admin_can_see_dashboard_spec.rb
@@ -34,4 +34,37 @@ feature "A visitor logging in" do
     visit "/admin/dashboard"
     expect(page).to have_content("The page you were looking for doesn't exist")
   end
+
+  scenario "can see all orders on dashboard if admin" do
+    User.create(username: "admin",
+                password: "password",
+                full_name: "Admin Adminerstein",
+                address: "123 Admin Blvd, Admintown, USA",
+                role: 1)
+    user = User.create(username: "Chris",
+                       password: "password",
+                       full_name: "Bob C",
+                       address: "9 Sherman",
+                       role: 1)
+    category = Category.create(name: "Cat Stuff")
+    loan = Loan.create(title: "Kitten Mittens",
+                       description: "Everyone needs them!",
+                       price: 100.5,
+                       category_id: category.id)
+    order = Order.create(user_id: user.id,
+                         card_number: "1234",
+                         card_expiration: "09/10",
+                         total_cost: 100.50,
+                         status: "Ordered")
+    OrderItem.create(loan_id: loan.id,
+                     quantity: 1,
+                     order_id: order.id)
+    visit "/"
+    click_link("Log In")
+    fill_in "user[username]", with: "admin"
+    fill_in "user[password]", with: "password"
+    click_button "Log In"
+    expect(page).to have_content("Order ID: 1")
+    expect(page).to have_content("Status: Ordered")
+  end
 end
