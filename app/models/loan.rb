b3f3b73f2a56a9c2d19ce4a2f58d384e3d2273f0
Jeffrey Ruane <ruane.jb@gmail.com>
2015-09-30 14:38:21 -0600

    Make changes for new s3 bucket

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -9,7 +9,7 @@ class Loan < ActiveRecord::Base
   belongs_to :category
   has_attached_file :avatar,
                     styles: { medium: "800x300#", thumb: "320x150#" },
-                    default_url: "https://s3-us-west-2.amazonaws.com/turingdenvestments/assets/Richard_Stallman_by_Anders_Brenna_01.jpg"
+                    default_url: "https://s3.amazonaws.com/turing-denvestments/assets/Richard_Stallman_by_Anders_Brenna_01.jpg"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
   def avatar_remote_url(url)
