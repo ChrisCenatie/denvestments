f8338e5f05059fe76bec78c2df393242c406a83c
Chris Cenatiempo <etothe2pieye@gmail.com>
2015-10-01 13:12:25 -0600

    updated order model test

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,40 +1,33 @@
 require "rails_helper"
 
 RSpec.describe Order, type: :model do
-  before(:each) do
-    @order = Order.new(user_id: 1,
-                       card_number: "1234123412341234",
-                       card_expiration: "09/09",
-                       status: "Ordered",
-                       total_cost: 150)
-  end
-
+  include_context "features"
   it "is valid" do
-    expect(@order).to be_valid
+    expect(order).to be_valid
   end
 
   it "is invalid without a card number" do
-    @order.card_number = nil
-    expect(@order).to_not be_valid
+    order.card_number = nil
+    expect(order).to_not be_valid
   end
 
   it "is invalid without a card expiration" do
-    @order.card_expiration = nil
-    expect(@order).to_not be_valid
+    order.card_expiration = nil
+    expect(order).to_not be_valid
   end
 
   it "is invalid without a status" do
-    @order.status = nil
-    expect(@order).to_not be_valid
+    order.status = nil
+    expect(order).to_not be_valid
   end
 
   it "is invalid with a word non-valid status" do
-    @order.status = "Good to Go"
-    expect(@order).to_not be_valid
+    order.status = "Good to Go"
+    expect(order).to_not be_valid
   end
 
   it "must belong to a user" do
-    @order.user_id = nil
-    expect(@order).to_not be_valid
+    order.user_id = nil
+    expect(order).to_not be_valid
   end
 end
