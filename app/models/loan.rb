e8ab7d33d7132be7296ccde0f693ea5d79230bc4
Jeffrey Ruane <ruane.jb@gmail.com>
2015-10-01 12:07:12 -0600

    Add sad path test for admin editting their own information

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -11,9 +11,4 @@ class Loan < ActiveRecord::Base
                     styles: { medium: "800x300#", thumb: "320x150#" },
                     default_url: "https://s3.amazonaws.com/turing-denvestments/assets/Richard_Stallman_by_Anders_Brenna_01.jpg"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
-
-  def avatar_remote_url(url)
-    self.avatar = URI.parse(url)
-    @avatar_remote_url
-  end
 end
